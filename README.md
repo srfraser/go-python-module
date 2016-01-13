5dc6b412a309d246132d864d20a1dc5ecbd42170
Simon Fraser <simonfr@gmail.com>
2016-01-13 10:47:29 +0000

    initial version, cribbed from tutorial

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # go-python-module
+
+
+Experimenting with building Go shared object files to 
+use as Python modules, based on:
+https://blog.filippo.io/building-python-modules-with-go-1-5/
+
+
+go build -buildmode=c-shared -o foo.so
+python3 -c 'import foo; print(foo.sum(2, 40))
+
+
---
+++ foo.h
@@ -0,0 +1,63 @@
+/* Created by "go tool cgo" - DO NOT EDIT. */
+
+/* package github.com/srfraser/go-python-module */
+
+/* Start of preamble from import "C" comments.  */
+
+
+#line 3 "/home/simon/.gvm/pkgsets/go1.5.1/global/src/github.com/srfraser/go-python-module/sum.go"
+
+ #define Py_LIMITED_API
+ #include <Python.h>
+ int PyArg_ParseTuple_LL(PyObject *, long long *, long long *);
+
+
+
+/* End of preamble from import "C" comments.  */
+
+
+/* Start of boilerplate cgo prologue.  */
+
+#ifndef GO_CGO_PROLOGUE_H
+#define GO_CGO_PROLOGUE_H
+
+typedef signed char GoInt8;
+typedef unsigned char GoUint8;
+typedef short GoInt16;
+typedef unsigned short GoUint16;
+typedef int GoInt32;
+typedef unsigned int GoUint32;
+typedef long long GoInt64;
+typedef unsigned long long GoUint64;
+typedef GoInt64 GoInt;
+typedef GoUint64 GoUint;
+typedef __SIZE_TYPE__ GoUintptr;
+typedef float GoFloat32;
+typedef double GoFloat64;
+typedef __complex float GoComplex64;
+typedef __complex double GoComplex128;
+
+// static assertion to make sure the file is being used on architecture
+// at least with matching size of GoInt.
+typedef char _check_for_64_bit_pointer_matching_GoInt[sizeof(void*)==64/8 ? 1:-1];
+
+typedef struct { char *p; GoInt n; } GoString;
+typedef void *GoMap;
+typedef void *GoChan;
+typedef struct { void *t; void *v; } GoInterface;
+typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
+
+#endif
+
+/* End of boilerplate cgo prologue.  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern PyObject* sum(PyObject* p0, PyObject* p1);
+
+#ifdef __cplusplus
+}
+#endif
